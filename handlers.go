415225d66d245752add7ab18f562fff3961bf69e
Ahsan Nasir <ahsan.nasir@sap.com>
2022-06-26 01:41:56 +0200

    seperate handlers

---
+++ handlers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	artifacts "ml-cicd/src/artifacts"
+	"ml-cicd/src/builder"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+func publisher(w http.ResponseWriter, r *http.Request) {
+	buildID := uuid.New().String()
+	if r.Method == "POST" {
+
+		err := artifacts.Upload(w, r, buildID)
+		if err != nil {
+			w.Write([]byte("An Error Occurred!"))
+		}
+		go builder.Build(buildID)
+
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"build_id": buildID})
+}
+
+func getlogger(w http.ResponseWriter, r *http.Request) {
+	keyVal := r.URL.Query()["build_id"][0]
+	w.Write([]byte(artifacts.Getlog(keyVal)))
+}
+
+func getstatus(w http.ResponseWriter, r *http.Request) {
+	keyVal := r.URL.Query()["build_id"][0]
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"build_status": builder.Status(keyVal)})
+}
