e0ed87652adf3baf1727d0c8ef934344961b2c59
Ahsan Nasir <ahsan.nasir@sap.com>
2022-06-26 14:14:24 +0200

    handlers improved

--- handlers.go
+++ handlers.go
@@ -3,7 +3,8 @@ package main
 import (
 	"encoding/json"
 	artifacts "ml-cicd/src/artifacts"
-	"ml-cicd/src/builder"
+	builder "ml-cicd/src/builder"
+	utils "ml-cicd/src/utilities"
 	"net/http"
 
 	"github.com/google/uuid"
@@ -15,7 +16,11 @@ func publisher(w http.ResponseWriter, r *http.Request) {
 
 		repository := r.FormValue("repository")
 		imagename := r.FormValue("name")
-		err := artifacts.Upload(w, r, buildID)
+		// find a file submitted as a form
+		file, handler, err := r.FormFile("Dockerfile")
+		defer file.Close()
+
+		err := artifacts.GenArtifacts(file, handler, buildID)
 		if err != nil {
 			w.Write([]byte("An Error Occurred!"))
 		}
@@ -28,11 +33,11 @@ func publisher(w http.ResponseWriter, r *http.Request) {
 
 func getlogger(w http.ResponseWriter, r *http.Request) {
 	keyVal := r.URL.Query()["build_id"][0]
-	w.Write([]byte(artifacts.Getlog(keyVal)))
+	w.Write([]byte(artifacts.FetchLog(keyVal)))
 }
 
 func getstatus(w http.ResponseWriter, r *http.Request) {
 	keyVal := r.URL.Query()["build_id"][0]
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"build_status": builder.Status(keyVal)})
+	json.NewEncoder(w).Encode(map[string]string{"build_status": utils.GetBuildStatus(keyVal)})
 }
