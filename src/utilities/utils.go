a975a8faa01973b8e859696d05cd1b0b2f5e3a7a
Ahsan Nasir <ahsan.nasir@sap.com>
2022-06-26 14:14:04 +0200

    utils added

---
+++ src/utilities/utils.go
@@ -0,0 +1,37 @@
+package utilities
+
+import (
+	"context"
+
+	"github.com/docker/docker/client"
+)
+
+var builds = map[string]string{}
+
+func SetBuildStatus(buildID string, flag int) {
+	switch flag {
+	case 0:
+		builds[buildID] = "running"
+	case 1:
+		builds[buildID] = "success"
+	case 2:
+		builds[buildID] = "failed"
+	}
+}
+
+func GetBuildStatus(buildID string) string {
+	return builds[buildID]
+}
+
+func GetBuildPath(buildID string) string {
+	return "./data/" + buildID
+}
+
+func GetDockerClient(ctx context.Context) *client.Client {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+
+	return cli
+}
