9f28253fb1dc74d75079c10691e390207247b241
Ahsan Nasir <ahsan.nasir@sap.com>
2022-06-26 02:41:21 +0200

    new variables for handlers

--- handlers.go
+++ handlers.go
@@ -13,11 +13,13 @@ func publisher(w http.ResponseWriter, r *http.Request) {
 	buildID := uuid.New().String()
 	if r.Method == "POST" {
 
+		repository := r.FormValue("repository")
+		imagename := r.FormValue("name")
 		err := artifacts.Upload(w, r, buildID)
 		if err != nil {
 			w.Write([]byte("An Error Occurred!"))
 		}
-		go builder.Build(buildID)
+		go builder.Build(buildID, repository, imagename)
 
 	}
 	w.Header().Set("Content-Type", "application/json")
