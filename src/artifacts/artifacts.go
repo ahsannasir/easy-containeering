fbcbb36e28b1c291473dde9926204d71d9f9fc9c
Ahsan Nasir <ahsan.nasir@sap.com>
2022-06-26 14:06:56 +0200

    artifacts improved

--- src/artifacts/artifacts.go
+++ src/artifacts/artifacts.go
@@ -1,47 +1,70 @@
 package artifacts
 
 import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
-	"net/http"
+	"mime/multipart"
 	"os"
+
+	er "ml-cicd/api/types"
+	utils "ml-cicd/src/utilities"
 )
 
-func Upload(w http.ResponseWriter, r *http.Request, buildID string) error {
-	file, handler, err := r.FormFile("Dockerfile")
-	defer file.Close()
+// GenArtifacts: This function generates a file adapted to custom mechanisms
+func GenArtifacts(file multipart.File, handler *multipart.FileHeader, buildID string) error {
 
-	if err != nil {
-		panic(err) //dont do this
-	}
-
-	// Create a Directory for build
-	os.MkdirAll("./data/"+buildID, os.ModePerm)
+	// Create a Directory for build if it doesn't exist
+	os.MkdirAll(utils.GetBuildPath(buildID), os.ModePerm)
 
 	// Create a dockerfile and replicate the one sent by user
-	f, err := os.OpenFile("./data/"+buildID+"/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0700)
+	f, err := os.OpenFile(utils.GetBuildPath(buildID)+"/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0700)
 	if err != nil {
-		panic(err) //please dont
+		return err //please dont
 	}
+
 	defer f.Close()
 	io.Copy(f, file)
 	return err
 }
 
-func Log(filename string, text string) {
-	f, err := os.OpenFile(filename+".txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		panic(err)
+// GetLog: This function allows reading logs for several running or complete builds
+func GenLog(rd io.Reader, path string) error {
+	var lastLine string
+
+	scanner := bufio.NewScanner(rd)
+	for scanner.Scan() {
+		lastLine = scanner.Text()
+		fmt.Println(scanner.Text())
+		f, err := os.OpenFile(path+".txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			panic(err)
+		}
+
+		defer f.Close()
+
+		if _, err = f.WriteString(scanner.Text() + "\n"); err != nil {
+			panic(err)
+		}
 	}
 
-	defer f.Close()
-
-	if _, err = f.WriteString(text); err != nil {
-		panic(err)
+	errLine := &er.ErrorLine{}
+	json.Unmarshal([]byte(lastLine), errLine)
+	if errLine.Error != "" {
+		return errors.New(errLine.Error)
 	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func Getlog(buildID string) string {
-	dat, err := os.ReadFile(buildID + ".txt")
+func FetchLog(buildID string) string {
+	dat, err := os.ReadFile("./data/" + buildID + "/" + buildID + ".txt")
 	if err != nil {
 		panic(err)
 	}
