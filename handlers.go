bad9d8a633ad8122e48e27ff1db4e770669275ae
Ahsan Nasir <ahsan.nasir@sap.com>
2022-06-27 15:37:55 +0200

    cleanup of handlers

--- handlers.go
+++ handlers.go
@@ -12,41 +12,65 @@ import (
 	"github.com/google/uuid"
 )
 
+// publisher: An API handler that allows consumers to publish their dockerfiles
+// from where a docker image is built and pushed onto a docker registry
 func publisher(w http.ResponseWriter, r *http.Request) {
+
+	// generate a guid to identify builds
 	buildID := uuid.New().String()
+
+	// this ensures publisher only works if user is authenticated
 	auth.Verify(w, r)
 	if r.Method == "POST" {
 
+		// fetch form fields
 		repository := r.FormValue("repository")
 		imagename := r.FormValue("name")
-		// find a file submitted as a form
 		file, handler, err := r.FormFile("Dockerfile")
 		defer file.Close()
-		if err != nil || file == nil {
-			w.Write([]byte("An Error Occurred!"))
+
+		// return bad request if invalid inputs are passed
+		if err != nil || file == nil || repository == "" || imagename == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
 		}
+
+		// finally generate artifacts that are required to start the build process
 		err = artifacts.GenArtifacts(file, handler.Filename, buildID)
 
 		// get a client to docker daemon
-		ctx := context.Background()
-		cli, _ := utils.GetDockerClient(ctx)
+		cli, err := utils.GetDockerClient(context.Background())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+
+		// Finally initiate the build job. The build job executes as a go-routine
+		// that unblocks the user from waiting for this API endpoint to return
+		// and builds & pushes the image to registry concurrently.
+		// To fetch logs or status of this concurrent build job, user can use /api/logs or /api/status endpoints.
 		go builder.Build(cli, buildID, repository, imagename)
 
 	}
+	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"build_id": buildID})
 }
 
+// getLogger: An API handler that allows fetching the logs of a particular build process
+// logs can be fetched during the build run or after the build has succeeded
 func getlogger(w http.ResponseWriter, r *http.Request) {
 	keyVal := r.URL.Query()["build_id"][0]
 	logs, err := artifacts.FetchLog(keyVal)
 	if err != nil {
-		w.Write([]byte("An Error Occurred"))
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
 	}
 	w.Write([]byte(logs))
-
 }
 
+// getstatus: An API handler that fetches the status of concurrent build.
+// Build Status can be "running", "failed" or "success" depending upon the build progress.
 func getstatus(w http.ResponseWriter, r *http.Request) {
 	keyVal := r.URL.Query()["build_id"][0]
 	w.Header().Set("Content-Type", "application/json")
--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-// “In your company there are many data scientists, who train deep learning models. They use different ML frameworks such as TensorFlow, PyTorch, scikit-learn, etc.
-// The data scientists need to jointly use and share the limited ML-accelerated hardware such as GPUs. Containers seem like an awesome way to encapsulate their custom
-// model training scripts and their library dependencies. The data scientists are comfortable working with Docker containers. You need to design and implement a simple
-// RESTful service that accepts a Dockerfile from the user. The service builds a Docker image out of the Dockerfile and pushes it to a Docker registry. Note that the
-// build might take some time. For the purpose of this assignment, it can be assumed that the service and Docker daemon run on the same machine.”
 import (
 	"fmt"
 	"log"
@@ -13,21 +8,27 @@ import (
 	auth "ml-cicd/auth"
 )
 
+// “In your company there are many data scientists, who train deep learning models. They use different ML frameworks such as TensorFlow, PyTorch, scikit-learn, etc.
+// The data scientists need to jointly use and share the limited ML-accelerated hardware such as GPUs. Containers seem like an awesome way to encapsulate their custom
+// model training scripts and their library dependencies. The data scientists are comfortable working with Docker containers. You need to design and implement a simple
+// RESTful service that accepts a Dockerfile from the user. The service builds a Docker image out of the Dockerfile and pushes it to a Docker registry. Note that the
+// build might take some time. For the purpose of this assignment, it can be assumed that the service and Docker daemon run on the same machine.”
+
 func main() {
 
-	fmt.Println("Welcome to the ML CICD Service!")
+	fmt.Println("Welcome to the ML-CICD Service!")
 
+	// Authentication
+	// ensures a secure authentication mechanism in API
+	http.HandleFunc("/api/signin", auth.Signin)
+
+	// Image Builder and Pushers
 	// publishes a build job
 	http.HandleFunc("/api/publish", publisher)
-
 	// returns build logs of a completed or a running build
 	http.HandleFunc("/api/logs", getlogger)
-
 	// returns build statuses
 	http.HandleFunc("/api/status", getstatus)
-
-	// Sign IN
-	http.HandleFunc("/api/signin", auth.Signin)
 
 	initiateServer()
 }
@@ -40,7 +41,7 @@ func initiateServer() {
 	}
 }
 
-// logRequest: logout API calls to the stdout
+// logRequest: logs API calls to the stdout
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
